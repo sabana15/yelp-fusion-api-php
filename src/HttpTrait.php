b2012507aadc4a1712eb3cbc6770a61acc123fa1
sabana15 <sabana_tai@yahoo.com>
2021-06-17 18:19:58 +1000

    added comments

--- src/HttpTrait.php
+++ src/HttpTrait.php
@@ -18,18 +18,32 @@ trait HttpTrait
     protected $httpClient;
     protected $scheme;
 
-   
+    /**
+    * Get Http Client
+    * @return  \GuzzleHttp\Client
+    */
     public function getHttpClient()
     {
         return $this->httpClient;
     }
 
+    /**
+    * Set Http Client
+    * @param  \GuzzleHttp\Client
+    */
     public function setHttpClient(HttpClient $client)
     {
         $this->httpClient = $client;
         return $this;
     }
-
+    
+    /**
+    * Updates the URL with query strings
+    * @param string url
+    * @param array parameters
+    * @param array optional parameters
+    * @return string
+    */
     protected function appendUrlwithParams($url, array $parameters = array(), array $options = array())
     {
         $url = rtrim($url, '?');
@@ -45,6 +59,10 @@ trait HttpTrait
         return $url;
     }
 
+    /**
+    * Process the parametes to set as query string
+    * @return  string
+    */
     protected function prepareQueryParams($params = [], $arrayParams = [])
     {
         array_walk($params, function ($value, $key) use (&$params, $arrayParams) {
@@ -63,6 +81,10 @@ trait HttpTrait
         return http_build_query($params);
     }
 
+    /**
+    * Prepare the client request instance
+    * @return  Request
+    */
     public function getClientRequest($method, $uri, array $headers = [], $body = null, $version = '1.1')
     {
         $uri = new Uri($uri);
@@ -78,12 +100,23 @@ trait HttpTrait
         return new Request($method, $uri, $headers, $body, $version);
     }
 
+    /**
+     * Makes a request to the Yelp API 
+     * returns the response     *
+     * @param    RequestInterface $request
+     * @return  The JSON response
+     */
     protected function processClientRequest(RequestInterface $request)
     {
         $response = $this->getClientResponse($request);
         return json_decode($response->getBody());
     }
 
+    /**
+     * Sends a request interface and returns a response.
+     * @param  RequestInterface $request
+     * @return ResponseInterface
+     */
     public function getClientResponse(RequestInterface $request)
     {
         try {
